1ff8d45884ec749fe97e2cafb976feb121f4861c
cartario <cartario@yandex.ru>
2020-08-25 16:51:12 +0300

    Updates

---
+++ precache-manifest.241d14a11c534ea394ded832b5bfa4f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad47761291db5d5271baeca77d4121ba",
+    "url": "/aviabit/index.html"
+  },
+  {
+    "revision": "bc3e5274e7f4c8af3436",
+    "url": "/aviabit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a128dada7d9640bc102",
+    "url": "/aviabit/static/js/2.0e823d74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aviabit/static/js/2.0e823d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc3e5274e7f4c8af3436",
+    "url": "/aviabit/static/js/main.4eeb0d40.chunk.js"
+  },
+  {
+    "revision": "296d4798780a3ba6221a",
+    "url": "/aviabit/static/js/runtime-main.12673ce2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aviabit/static/media/logo.5d5d9eef.svg"
+  }
+]);
