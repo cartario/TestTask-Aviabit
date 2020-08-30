0aa7eb283ad72327ac0fb3ad1758f24d436e7cde
VasiliyMac <cartario@yandex.ru>
2020-08-30 13:25:12 +0300

    propTypes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import {HashRouter, Route} from 'react-router-dom';
 import '../sass/app.scss';
 import Header from './header';
@@ -56,6 +57,37 @@ const mapDispatchToProps = (dispatch) => ({
     dispatch(ActionCreator.setCurrentFlights(value, year, isFactData));
   },
 });
+
+App.propTypes = {
+  flights: PropTypes.arrayOf(
+    PropTypes.shape({
+      dateFlight: PropTypes.instanceOf(Date).isRequired,
+      flight: PropTypes.string.isRequired,
+      pinType: PropTypes.string.isRequired,
+      pin: PropTypes.string.isRequired,
+      timeFlight: PropTypes.number.isRequired,
+      timeBlock: PropTypes.number.isRequired,
+      timeNight: PropTypes.number.isRequired,
+      timeBiologicalNight: PropTypes.number.isRequired,
+      timeWork: PropTypes.number.isRequired,
+      type: PropTypes.number.isRequired,
+      takeoff: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        lat: PropTypes.number.isRequired,
+        long: PropTypes.number.isRequired,
+      }).isRequired,
+      landing: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        lat: PropTypes.number.isRequired,
+        long: PropTypes.number.isRequired,
+      }).isRequired,
+    }).isRequired,
+  ),
+  setActiveFlight: PropTypes.func.isRequired,
+  isFactData: PropTypes.bool.isRequired,
+	toggleHandler: PropTypes.func.isRequired,
+  setCurrentFlights: PropTypes.func.isRequired,
+};
 
 export {App};
 export default connect(mapStateToProps, mapDispatchToProps)(withToggle(App));
