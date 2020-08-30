179a3ed833ec1629b1e35406a0b7fc63ab7985f0
VasiliyMac <cartario@yandex.ru>
2020-08-30 15:48:07 +0300

    ...

--- src/components/checkbox.jsx
+++ src/components/checkbox.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Checkbox = (props) => {
   const {isFactData} = props;
   return (
-    <div>          
+    <div className="checkbox-container">          
       <input className="checkbox" type="checkbox" id="checkbox" onChange={props.toggleHandler} autoFocus checked={isFactData}/>
       <label className="checkbox__label" htmlFor="checkbox">По факту</label>
     </div> 
